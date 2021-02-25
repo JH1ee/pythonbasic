9a46c4e80bb35f0ebb927308a3445b521920a273
JH1ee <78770290+JH1ee@users.noreply.github.com>
2021-02-25 17:44:54 +0900

    Add files via upload

---
+++ classday9/vowels3.py
@@ -0,0 +1,12 @@
+vowels = ['a','e','i','o','u']
+
+word = input("provide a word to search : ")
+found = []
+
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+
+for vowel in found:
+    print(vowel)
